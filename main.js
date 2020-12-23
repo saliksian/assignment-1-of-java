38d939fe145bfa6776a983efda7362f72db5bd28
saliksian <salikafzal91@gmail.com>
2020-12-23 21:03:42 +0500

    Visa Eligibility
    Tells about you are eligible or not Visa

---
+++ main.js
@@ -0,0 +1,16 @@
+// Visa Eligible system
+let age;
+age =24;
+
+if (age >= 20 && age <= 60){
+    console.log("You are Young Citizen");
+    console.log("You are Eligible for visa!");
+}
+else if(age>60){
+    console.log("You are Senior Citizen");
+    console.log("You are Eligible for visa!");
+}
+else if(age<=1){
+    console.log("You are Child Citizen");
+    console.log(" You are  Not Eligible for visa!");
+}
